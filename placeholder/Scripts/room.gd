030e7e0b9a099328ac422c50928573a3fb70ef2b
rekomo <rekomo.code@protonmail.com>
2024-12-15 07:36:09 +0100

    asd

--- placeholder/Scripts/room.gd
+++ placeholder/Scripts/room.gd
@@ -11,12 +11,6 @@ func _ready() -> void:
 		var scene = s.instantiate()
 		scene.input_event.connect(_on_stolek_input_event)
 		self.add_child(scene)
-	if room_name == "room2" and LoadingScene.get_state(room_name) != null:
-		if LoadingScene.get_state(room_name)["stolek"] == "placed":
-			var s = preload("res://Scenes/stolek.tscn")
-			var scene = s.instantiate()
-			self.add_child(scene)
-			scene.position.x = 1308
 
 func handleItemDropped(zoneName):
 	if zoneName == "chair":
@@ -27,13 +21,16 @@ func handleItemDropped(zoneName):
 		var s = preload("res://Scenes/stolek.tscn")
 		var scene = s.instantiate()
 		self.add_child(scene)
-		scene.position.x = 1308
 	else:
 		print("IT IS THE WRONG ONEEEEEEEEE")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if room_name == "room2" and LoadingScene.get_state(room_name) != null:
+		if LoadingScene.get_state(room_name)["stolek"] == "placed":
+			var s = preload("res://Scenes/stolek.tscn")
+			var scene = s.instantiate()
+			self.add_child(scene)
 
 
 func _on_left_door_body_entered(body: Node2D) -> void:
