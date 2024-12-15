15456f55c3ce14bc3916104442c8aa23ae6d4621
SonSku <sskuczen@student.agh.edu.pl>
2024-12-15 07:33:41 +0100

    clicker

--- placeholder/Scripts/clicker.gd
+++ placeholder/Scripts/clicker.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var progress_decrease_rate: float = 30.0  # Jak szybko pasek spada
+@export var progress_decrease_rate: float = 10.0  # Jak szybko pasek spada
 @export var progress_increase_amount: float = 5.0  # Jak dużo rośnie przy kliknięciu
 @export var max_progress: float = 100.0  # Maksymalna wartość paska
 @export var min_progress: float = 0.0  # Minimalna wartość paska
@@ -8,7 +8,7 @@ extends Node2D
 var progress: float = 0.0  # Aktualna wartość paska
 var game_won: bool = false  # Czy gra została wygrana
 
-@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var progress_bar: TextureProgressBar = $TextureProgressBar
 @onready var win_label: Label = $WinLabel  # Etykieta wygranej
 
 func _ready():
